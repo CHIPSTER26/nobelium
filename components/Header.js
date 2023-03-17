6282e7694fd5f984d2d9c0cef9adaea68208cd21
Christopher Huang <60949743+CHIPSTER26@users.noreply.github.com>
2023-03-17 19:27:05 +1100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -69,7 +69,7 @@ const Header = ({ navBarTitle, fullWidth }) => {
         <div className="flex items-center">
           <Link href="/">
             <a aria-label={BLOG.title}>
-              <div className="h-6 font-bold">
+              <div className="h-5 text-black">
                 {BLOG.title}
               </div>
             </a>
