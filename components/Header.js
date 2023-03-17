d9bb7e5a4ca105c5c39bbb3dc60cc2698f4ac89a
Christopher Huang <60949743+CHIPSTER26@users.noreply.github.com>
2023-03-17 18:39:50 +1100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -70,7 +70,29 @@ const Header = ({ navBarTitle, fullWidth }) => {
           <Link href="/">
             <a aria-label={BLOG.title}>
               <div className="h-6">
-                <p> Chris's Blog </p>
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <p> Chris's Blog </p>
+                  <rect width="24" height="24" fill="url(#paint0_radial)" />
+                  <defs>
+                    <radialGradient
+                      id="paint0_radial"
+                      cx="0"
+                      cy="0"
+                      r="1"
+                      gradientUnits="userSpaceOnUse"
+                      gradientTransform="rotate(45) scale(39.598)"
+                    >
+                      <stop stopColor="#CFCFCF" stopOpacity="0.6" />
+                      <stop offset="1" stopColor="#E9E9E9" stopOpacity="0" />
+                    </radialGradient>
+                  </defs>
+                </svg>
               </div>
             </a>
           </Link>
