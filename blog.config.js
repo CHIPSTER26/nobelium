b4951077a10f6a17dfe8c9954b89a13179da47c5
Christopher Huang <60949743+CHIPSTER26@users.noreply.github.com>
2023-03-17 18:16:48 +1100

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
   showAbout: true,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
-  ogImageGenerateURL: '', // The link to generate OG image, don't end with a slash
+  ogImageGenerateURL: 'https://en.gravatar.com/christopherhuang26gmailcom', // The link to generate OG image, don't end with a slash
   socialLink: '',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
