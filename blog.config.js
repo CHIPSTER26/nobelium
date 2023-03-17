ebff70aa7bb927eee5a29d67035c7fde178bef41
Christopher Huang <60949743+CHIPSTER26@users.noreply.github.com>
2023-03-17 14:41:55 +1100

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,6 +1,6 @@
 const BLOG = {
   title: "Chris's Blog",
-  author: "Chris's Blog",
+  author: "Christopher Huang",
   email: 'christopher.huang26@gmail.com',
   link: 'https://www.linkedin.com/in/christopher-huang-675ab329/',
   description: '',
