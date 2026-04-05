99ed4db8320c21ca8b9f28f85010790462cb0307
Christopher Huang <60949743+CHIPSTER26@users.noreply.github.com>
2026-04-06 00:23:50 +1000

    Update notionPageId to use environment variable

--- blog.config.js
+++ blog.config.js
@@ -23,8 +23,7 @@ const BLOG = {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
-  //notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
-  notionPageId: 6df61363816341b38e07d50d3dc2c92d, // DO NOT CHANGE THIS！！！
+  notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
   analytics: {
     provider: '', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
